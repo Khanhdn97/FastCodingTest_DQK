7bee16055fce2f6053f4796da861d8edebc6275c
Khanh197 <khanhquangdinh197@gmail.com>
2023-02-13 12:42:05 +0700

    new code

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const BarButton = document.querySelector("#bars");
+const HeaderList = document.querySelector(".header__list");
+const Actived = "actived";
+window.addEventListener("click", function (e) {
+    if (!HeaderList.contains(e.target)
+        && HeaderList.classList.contains(Actived) === true
+        && !BarButton.contains(e.target)) {
+        HeaderList.classList.remove(Actived)
+        console.log("close")
+    }
+})
